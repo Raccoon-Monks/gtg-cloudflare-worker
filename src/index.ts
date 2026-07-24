77648f3681756a4bfe7bdade5caf2bcc5e8afc28
Leonardo Lourenço Crespilho <leonardo.lourenco@monks.com>
2026-07-23 22:54:34 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,29 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+interface Env {
+	GTG_KV: KVNamespace;
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
+		const t0 = performance.now();
+		await env.GTG_KV.get('minha_chave');
+		const tGet = Math.round(performance.now() - t0);
+		const hostname = new URL(request.url).hostname;
+		console.log('> hostname:', hostname);
+		const headers = new Headers();
+		headers.append('Server-Timing', `kvget;dur=${tGet}`);
+		headers.append(
+			'X-Forwarded-CountryRegion',
+			request.cf?.country && request.cf?.regionCode ? `${request.cf?.country}-${request.cf?.regionCode}` : 'unknown',
+		);
+		headers.append('X-Forwarded-Country', request.cf?.country || 'unknown');
+		headers.append('X-Forwarded-Region', request.cf?.regionCode || 'unknown');
+		const city = (request.cf?.city || 'unknown').normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+		headers.append('X-Forwarded-Geolocation', `latlong=${request.cf?.latitude},${request.cf?.longitude};city=${city}`);
+		headers.append('X-Gclb-Country', request.cf?.country || 'unknown');
+		headers.append('X-Gclb-Region', `${request.cf?.country}${request.cf?.regionCode}` || 'unknown');
+		return new Response('Hello Worker!', { headers });
 	},
 } satisfies ExportedHandler<Env>;
